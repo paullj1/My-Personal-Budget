4da4b4b0b55d104d23f2875e10a33ae4123806dc
Paul Jordan <paullj1@gmail.com>
2016-10-27 07:04:32 -0500

    Hopefully fixing CSRF token issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_My-Personal-Budget_session'
+Rails.application.config.session_store :cookie_store, key: "_rails_session_#{Rails.env}", domain: :all
+#Rails.application.config.session_store :cookie_store, key: '_My-Personal-Budget_session'
