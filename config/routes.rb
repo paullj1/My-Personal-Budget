b1af643a110681526c5578dba1e553bf21ef15f9
Paul Jordan <paullj1@gmail.com>
2016-10-31 09:15:49 -0500

    Removing users_controller and associated assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  get '/profile' => 'users#show', as: :profile
 
   post '/budgets/:id/share' => 'budgets#share', as: 'share_budget'
   post '/budgets/:id/unshare' => 'budgets#unshare', as: 'unshare_budget'
