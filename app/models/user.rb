d990db1e9c659716a9b33dae197a821df93aa9fd
Paul Jordan <paullj1@gmail.com>
2016-11-13 08:00:17 -0600

    Cleaning up devise config; adding 'confirmable' and 'lockable' to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :lockable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :budget, :join_table => :users_budgets
 
