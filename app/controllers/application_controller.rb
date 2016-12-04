ef6354f41590e145911a2fabf46a087a425a3c40
Paul Jordan <paullj1@gmail.com>
2016-12-04 15:48:30 -0600

    Add json support to devise; don't worry about CSRF token for JSON req's

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true, with: :exception
   before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  protected
+
+    def json_request?
+      request.format.json?
+    end
 end
