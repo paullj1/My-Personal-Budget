95fb2b9ff7d7e0e8fab86313a249b96a42484d28
Paul Jordan <paullj1@gmail.com>
2019-07-13 18:50:39 -0500

    Add transfer function to transactions

--- app/assets/javascripts/transactions.coffee
+++ app/assets/javascripts/transactions.coffee
@@ -1,3 +1,16 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+setup_hooks = ->
+  if !document.getElementById('transaction-type-selector') 
+    return
+
+  $('input[type=radio]').change ->
+    if this.value == 'Transfer'
+      $("#transfer-to-budget").show 100
+    else
+      $("#transfer-to-budget").hide 100
+    return
+
+$(document).on('turbolinks:load', setup_hooks)
--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -2,7 +2,7 @@ class TransactsController < ApplicationController
   before_action :owner?, only: [:show, :edit, :update, :destroy]
 
   def show
-		@transact = Transact.find(params[:id])
+    @transact = Transact.find(params[:id])
 
     respond_to do |format|
       format.html
@@ -12,6 +12,8 @@ class TransactsController < ApplicationController
 
   def new
     @transact = Transact.new
+
+    @new_transact = true
     @budgets = current_user.html_budgets
 
     respond_to do |format|
@@ -21,30 +23,93 @@ class TransactsController < ApplicationController
   end
 
   def create
-		@transact = Transact.new(transact_params)
-    @transact.user_id = current_user.id
+    @t_params = transact_params
     @budgets = current_user.html_budgets
-		if @transact.save
-    	flash[:success] = "Successfully submitted new transaction!"
 
-      respond_to do |format|
-        format.html { redirect_to root_url }
-        format.json { render json: {}, status: :created }
+    # Transfer
+    if @t_params[:credit] == 'Transfer'
+
+      @t_params[:credit] = false
+      @from_transact = Transact.new(@t_params)
+      @from_transact.user_id = current_user.id
+
+      @t_params[:credit] = true
+      @to_transact = Transact.new(@t_params)
+      @to_transact.budget_id = params[:to_budget_id]
+      @to_transact.user_id = current_user.id
+
+      if @to_transact.budget_id != @from_transact.budget_id and @to_transact.save and @from_transact.save
+        flash[:success] = "Successfully submitted new transaction!"
+
+        respond_to do |format|
+          format.html { redirect_to root_url }
+          format.json { render json: {}, status: :created }
+        end
+
+      else
+        @to_transact.user_id = nil # lets form hide the delete button
+        @from_transact.user_id = nil # lets form hide the delete button
+
+        if @to_transact.budget_id == @from_transact.budget_id
+          flash[:error] = "Cannot transfer to/from the same budget!"
+          @transact = Transact.new(@t_params)
+          @new_transact = true
+        end
+
+        respond_to do |format|
+          format.html { render 'new' }
+          format.json { render json: { transact: @transact, budgets: @budgets }, status: :unprocessable_entity }
+        end
       end
 
-		else
-      @transact.user_id = nil # lets form hide the delete button
+    # Normal single transaction
+    elsif @t_params[:credit] == 'Credit' or @t_params[:credit] == 'Debit'
+
+      if @t_params[:credit] == 'Credit'
+        @t_params[:credit] = true
+        puts @t_params
+        @transact = Transact.new(@t_params)
+
+      else # Debit
+        @t_params[:credit] = false
+        @transact = Transact.new(@t_params)
+      end
+
+      @transact.user_id = current_user.id
+
+      if @transact.save
+        flash[:success] = "Successfully submitted new transaction!"
+
+        respond_to do |format|
+          format.html { redirect_to root_url }
+          format.json { render json: {}, status: :created }
+        end
+
+      else
+        @transact.user_id = nil # lets form hide the delete button
+
+        respond_to do |format|
+          format.html { render 'new' }
+          format.json { render json: { transact: @transact, budgets: @budgets }, status: :unprocessable_entity }
+        end
+      end
+
+    # Unrecognized transaction
+    else
+      flash[:error] = "Unrecognized transaction type: #{@t_params[:credit]}"
 
       respond_to do |format|
         format.html { render 'new' }
         format.json { render json: { transact: @transact, budgets: @budgets }, status: :unprocessable_entity }
       end
-		end
+
+    end
   end
 
   def edit
-		@transact = Transact.find(params[:id])
+    @transact = Transact.find(params[:id])
     @budgets = current_user.html_budgets
+    @new_transact = false
 
     respond_to do |format|
       format.html
@@ -53,27 +118,39 @@ class TransactsController < ApplicationController
   end
 
   def update
-		@transact = Transact.find(params[:id])
-		if @transact.update_attributes(transact_params)
-    	flash[:success] = "Successfully updated transaction!"
+    @transact = Transact.find(params[:id])
+    @t_params = transact_params
 
-      respond_to do |format|
-        format.html { redirect_to root_url }
-        format.json { render json: {}, status: :accepted }
-      end
-		else
+    if @t_params[:credit] == 'Credit' or @t_params[:credit] == 'Debit'
 
-      respond_to do |format|
-        format.html { render 'edit' }
-        format.json { render json: @transact, status: :unprocessable_entity }
+      if @t_params[:credit] == 'Credit'
+        @t_params[:credit] = true
+      else
+        @t_params[:credit] = false
       end
 
-		end
+      if @transact.update_attributes(@t_params)
+        flash[:success] = "Successfully updated transaction!"
+
+        respond_to do |format|
+          format.html { redirect_to root_url }
+          format.json { render json: {}, status: :accepted }
+        end
+        return
+      end
+    end
+
+    flash[:error] = "Unrecognized options..."
+    respond_to do |format|
+      format.html { render 'edit' }
+      format.json { render json: @transact, status: :unprocessable_entity }
+    end
+
   end
 
   def destroy
     transact = Transact.find(params[:id])
-		transact.destroy
+    transact.destroy
     flash[:success] = "Transaction deleted!"
 
     respond_to do |format|
@@ -83,19 +160,19 @@ class TransactsController < ApplicationController
   end
 
   private
-		def transact_params
-			params.require(:transact).permit(:description, :credit, :amount, :budget_id)
-		end
+    def transact_params
+      params.require(:transact).permit(:description, :credit, :amount, :budget_id)
+    end
 
     def owner?
       owners = Transact.find(params[:id]).budget.user_ids
       unless owners.include? current_user.id
-				flash[:danger] = "Access denied!"
+        flash[:danger] = "Access denied!"
         respond_to do |format|
           format.html { redirect_to root_url }
           format.json { render json: {}, status: :forbidden }
         end
-			end
+      end
     end
 
 end
